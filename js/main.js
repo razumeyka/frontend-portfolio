687cc56bd8f8e78694e26c577d3c89d02c04c383
razumeyka <33911265+razumeyka@users.noreply.github.com>
2021-09-13 23:33:07 +0300

    ready

---
+++ js/main.js
@@ -0,0 +1,5 @@
+$(document).ready(function(){
+	
+
+}); 
+//# sourceMappingURL=data:application/json;charset=utf8;base64,eyJ2ZXJzaW9uIjozLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiIiwic291cmNlcyI6WyJtYWluLmpzIl0sInNvdXJjZXNDb250ZW50IjpbIiQoZG9jdW1lbnQpLnJlYWR5KGZ1bmN0aW9uKCl7XHJcblx0XHJcblxyXG59KTsgIl0sImZpbGUiOiJtYWluLmpzIn0=
